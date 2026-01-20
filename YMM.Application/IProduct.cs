9a6a178f5aee7127cd181e922057dee6c4308a5c
yousefmohsen <yousefmohsen232@gmail.com>
2026-01-20 07:45:44 +0200

    refactor files

---
+++ YMM.Application/IProduct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YMM.Application
+{
+    public interface IProduct
+    {
+    }
+}
--- YMM/Program.cs
+++ YMM/Program.cs
@@ -11,17 +11,18 @@ builder.Services.AddDbContext<AppDb>(option =>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
