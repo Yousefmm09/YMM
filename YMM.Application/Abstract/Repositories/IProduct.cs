6c3755bf175bb50657709d3347e3baa6babc354e
yousefmohsen <yousefmohsen232@gmail.com>
2026-01-23 14:38:06 +0200

    add repo and service of product

---
+++ YMM.Application/Abstract/Repositories/IProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMM.Application.Abstract
+{
+    internal interface IProduct
+    {
+    }
+}
---
+++ YMM.Application/Abstract/Services/IProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMM.Application.Dto.Product;
+using YMM.Data.Entities;
+
+namespace YMM.Application.Abstract.Services
+{
+    public interface IProduct
+    {
+        Task<AddProductDto> Add(AddProductDto dto);
+        Task<AddProductDto> Update(AddProductDto dto);
+        Task<string> Delete(int productId);
+        Task<List<Product>> GetAll();
+    }
+}
