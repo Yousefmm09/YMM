c2e65df65893ac8ac89718336e10614052876546
yousefmohsen <yousefmohsen232@gmail.com>
2026-02-02 13:29:33 +0200

    Update in Relation between Order and Rating v2

---
+++ YMM.Infrastructure/Migrations/20260202112636_Remove_RelationBetweenOrderAndReview.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YMM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_RelationBetweenOrderAndReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Orders_OrderId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_OrderId",
+                table: "Reviews");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_OrderId",
+                table: "Reviews",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Orders_OrderId",
+                table: "Reviews",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
